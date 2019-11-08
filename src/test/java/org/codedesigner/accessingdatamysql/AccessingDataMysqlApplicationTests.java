0271bbb00d7484f5d0ac172c4bf3610631da20e0
Michal Bližňák <michal.bliznak@gmail.com>
2019-11-08 20:35:17 +0100

    Initial release.

---
+++ src/test/java/org/codedesigner/accessingdatamysql/AccessingDataMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codedesigner.accessingdatamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
